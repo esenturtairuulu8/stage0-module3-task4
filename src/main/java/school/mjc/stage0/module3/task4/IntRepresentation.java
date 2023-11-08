8aa598930ccfc584f5ef1c671471c8fee6b6cc40
esenturtairuulu8 <esenturtairuulu8@gmail.com>
2023-11-08 10:10:00 +0600

    fixed

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char char1 = 'c';
+        char char2 = 'h';
+        char char3 = 'a';
+        char char4 = 'r';
+
+        System.out.println((int) char1);
+        System.out.println((int) char2);
+        System.out.println((int) char3);
+        System.out.println((int) char4);
     }
 }
+
